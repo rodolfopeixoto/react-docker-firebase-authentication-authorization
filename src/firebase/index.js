4879c2eb5fac4961cf88d6951bec4c418bb73618
Rodolfo Peixoto <rdxinho@gmail.com>
2018-01-09 12:39:02 -0200

    Created firebase's authentication api

---
+++ src/firebase/index.js
@@ -0,0 +1,22 @@
+import * as auth from './auth';
+import * as firebase from 'firebase';
+require('dotenv').config()
+const config = {
+  apiKey= process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+
+if(!firebase.apps.length){
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+  auth,
+  firebase,
+};
