dba9a14e449816669df50f618e7a074d45203937
Rodolfo Peixoto <rdxinho@gmail.com>
2018-01-10 12:53:42 -0200

    Added REACT_APP_ in environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,16 @@ import * as firebase from 'firebase';
 require('dotenv').config()
 
 const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
+  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
-console.log("API: ",process.env.API_KEY);
+
+// console.log(config);
+console.log(process.env);
 
 if(!firebase.apps.length){
   firebase.initializeApp(config);
